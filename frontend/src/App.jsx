a197e75f7f68982b8e7fc60e9b34293a8e127472
Fanpeng Li <serforfp@gmail.com>
2024-04-01 12:52:42 +0100

    Created frontend pages and setted background with aceternity UI

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,24 @@
+import { Route, Routes } from "react-router-dom";
+import SignUpPage from "../pages/SignUpPage";
+import NotFoundPage from "../pages/NotFoundPage";
+import LoginPage from "../pages/LoginPage";
+import HomePage from "../pages/HomePage";
+import TransactionPage from "../pages/TransactionPage";
+import Header from "./components/ui/Header";
+
 function App() {
+  const authUser = true;
   return (
     <>
-      <h1 className="text-red-500">Hello</h1>
+      {authUser && <Header />}
+
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/transaction/:id" element={<TransactionPage />} />
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
     </>
   );
 }
